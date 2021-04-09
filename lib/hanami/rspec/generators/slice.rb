02fe8aedbc5d253e24ce89424ebbbf625b2e6534
Luca Guidi <me@lucaguidi.com>
2021-04-09 12:39:12 +0200

    Add missing argument to `Hanami::CLI::Generators::Monolith::SliceContext`

--- lib/hanami/rspec/generators/slice.rb
+++ lib/hanami/rspec/generators/slice.rb
@@ -12,7 +12,7 @@ module Hanami
           @inflector = inflector
         end
 
-        def call(slice, context: Hanami::CLI::Generators::Monolith::SliceContext.new(inflector, nil, slice)) # rubocop:disable Metrics/AbcSize
+        def call(slice, context: Hanami::CLI::Generators::Monolith::SliceContext.new(inflector, nil, slice, nil)) # rubocop:disable Metrics/AbcSize
           fs.write("spec/#{slice}/action_spec.rb", t("action_spec.erb", context))
           fs.write("spec/#{slice}/view_spec.rb", t("view_spec.erb", context))
           fs.write("spec/#{slice}/repository_spec.rb", t("repository_spec.erb", context))
