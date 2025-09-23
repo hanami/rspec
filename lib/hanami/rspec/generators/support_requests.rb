45ae60a3342fc0435339db98df61239b23a6cd30
Aaron Allen <hello@aaronmallen.me>
2025-09-23 03:55:22 -0500

    Fix `support_request` template (#40)
    This fixes the `support_request` template to respect rubocop's
    RSpec/ContextWording cop.
    
    https://docs.rubocop.org/rubocop-rspec/cops_rspec.html#rspeccontextwording

--- lib/hanami/rspec/generators/support_requests.rb
+++ lib/hanami/rspec/generators/support_requests.rb
@@ -2,12 +2,12 @@
 
 require "rack/test"
 
-RSpec.shared_context "Rack::Test" do
+RSpec.shared_context "with Rack::Test" do
   # Define the app for Rack::Test requests
   let(:app) { Hanami.app }
 end
 
 RSpec.configure do |config|
   config.include Rack::Test::Methods, type: :request
-  config.include_context "Rack::Test", type: :request
+  config.include_context "with Rack::Test", type: :request
 end
--- spec/unit/hanami/rspec/commands/install_spec.rb
+++ spec/unit/hanami/rspec/commands/install_spec.rb
@@ -220,14 +220,14 @@ RSpec.describe Hanami::RSpec::Commands::Install do
 
         require "rack/test"
 
-        RSpec.shared_context "Rack::Test" do
+        RSpec.shared_context "with Rack::Test" do
           # Define the app for Rack::Test requests
           let(:app) { Hanami.app }
         end
 
         RSpec.configure do |config|
           config.include Rack::Test::Methods, type: :request
-          config.include_context "Rack::Test", type: :request
+          config.include_context "with Rack::Test", type: :request
         end
       EOF
       expect(fs.read("spec/support/requests.rb")).to eq(support_requests)
